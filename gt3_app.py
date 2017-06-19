21e6c9317ae05c7c0eb355871a4685789dd9e8a2
flowsnow <snowlight437@gmail.com>
2017-06-19 14:35:51 +0800

    捕获异常报错信息并且输出到日志中

--- gt3_app.py
+++ gt3_app.py
@@ -18,30 +18,35 @@ import datetime
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s [%(threadName)s] %(message)s',
                     filename='gt3_app.log', filemode='a')
 
+
 def start():
-    logging.info('========================================================')
-    logging.info('TIMESTAMP: {}'.format(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
-    config = configparser.ConfigParser()
-    config.read(filenames='config.ini', encoding='utf-8')
-    excel = Excel(config)
+    try:
+        logging.info('========================================================')
+        logging.info('TIMESTAMP: {}'.format(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
+        config = configparser.ConfigParser()
+        config.read(filenames='config.ini', encoding='utf-8')
+        excel = Excel(config)
 
-    # handle database
-    logging.info('starting to handle database items')
-    dh = DatabasesHandler(excel, config)
-    dh.run()
-    logging.info('handle database items finished')
+        # handle database
+        logging.info('starting to handle database items')
+        dh = DatabasesHandler(excel, config)
+        dh.run()
+        logging.info('handle database items finished')
 
-    # handle files
-    logging.info('starting to handle all ip.log file')
-    fh = FilesHandler(excel, config)
-    fh.run()
-    logging.info('handle all ip.log finished')
+        # handle files
+        logging.info('starting to handle all ip.log file')
+        fh = FilesHandler(excel, config)
+        fh.run()
+        logging.info('handle all ip.log finished')
 
-    logging.info('starting to save excel')
-    excel.save()
-    logging.info('save excel finished')
+        logging.info('starting to save excel')
+        excel.save()
+        logging.info('save excel finished')
 
-    backup(config)
+        backup(config)
+    except Exception as e:
+        logging.exception(e)
+        raise e
 
 
 if __name__ == '__main__':
